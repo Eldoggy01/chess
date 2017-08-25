560aa6c4004b6ef59925054f8428eb835a831482
Иван Бонькин <i.bonkin@pflb.ru>
2017-08-25 15:53:15 +0300

    added alphabeta

--- src/main/java/ru/pflb/chess/Search.java
+++ src/main/java/ru/pflb/chess/Search.java
@@ -2,10 +2,36 @@ package ru.pflb.chess;
 
 import java.util.List;
 
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+import static java.util.Collections.emptyList;
+import static ru.pflb.chess.Color.WHITE;
+
 /**
  * @author <a href="mailto:8445322@gmail.com">Ivan Bonkin</a>.
  */
 public class Search {
+
+    class Entry implements Comparable<Integer> {
+        int value;
+        // principal variation
+        List<Move> pv;
+
+        public Entry(int value, List<Move> pv) {
+            this.value = value;
+            this.pv = pv;
+        }
+
+        public List<Move> addPv(Move mv) {
+            pv.add(mv);
+            return pv;
+        }
+
+        @Override
+        public int compareTo(Integer other) {
+            return Integer.compare(value, other);
+        }
+    }
 
     public static int perft(Board board, int depth) {
         if (depth == 0) {
@@ -23,24 +49,52 @@ public class Search {
         return positions;
     }
 
-    public static Move search(Board board) {
-        List<Move> moves = new MoveGenerator(board).generateMoves();
-        int maxEval = -10000;
-        Move bestMove = null;
-        for (Move move : moves) {
-            board.doMove(move);
-            // TODO - исключить недопустимые ходы
-            int eval = alfabeta(-10000, +10000);
-            if (eval > maxEval) {
-                bestMove = move;
-                maxEval = eval;
-            }
-            board.undoMove(move);
-        }
-        return bestMove;
+    public static List<Move> bestMoves(Board board) {
+        Search search = new Search();
+        return board.getSideToMove() == WHITE
+                ? search.alphaBetaMax(board, MIN_VALUE, MAX_VALUE, 2).pv :
+                search.alphaBetaMin(board, MIN_VALUE, MAX_VALUE, 2).pv;
     }
 
-    public static int alfabeta(int alfa, int beta) {
-        return 0;
+    private Entry alphaBetaMax(Board board, int alpha, int beta, int depthleft) {
+        if (depthleft == 0) {
+            return new Entry(new Eval(board).getValue(), emptyList());
+        }
+        List<Move> moves = new MoveGenerator(board).generateMoves();
+        for (Move move : moves) {
+            board.doMove(move);
+            Entry score = alphaBetaMin(board, alpha, beta, depthleft - 1);
+            board.undoMove(move);
+            if (score.compareTo(beta) >= 0) {
+                // fail hard beta-cutoff
+                return new Entry(beta, score.addPv(move));
+            }
+            if (score.compareTo(alpha) > 0) {
+                // alpha acts like max in MiniMax
+                alpha = score.value;
+            }
+        }
+        return new Entry(alpha, emptyList());
+    }
+
+    private Entry alphaBetaMin(Board board, int alpha, int beta, int depthleft) {
+        if (depthleft == 0) {
+            return new Entry(-new Eval(board).getValue(), emptyList());
+        }
+        List<Move> moves = new MoveGenerator(board).generateMoves();
+        for (Move move : moves) {
+            board.doMove(move);
+            Entry score = alphaBetaMax(board, alpha, beta, depthleft - 1);
+            board.undoMove(move);
+            if (score.compareTo(alpha) <= 0) {
+                // fail hard alpha-cutoff
+                return new Entry(alpha, score.addPv(move));
+            }
+            if (score.compareTo(beta) < 0) {
+                // beta acts like min in MiniMax
+                beta = score.value;
+            }
+        }
+        return new Entry(beta, emptyList());
     }
 }
--- src/test/java/ru/pflb/chess/SearchTest.java
+++ src/test/java/ru/pflb/chess/SearchTest.java
@@ -4,7 +4,12 @@ package ru.pflb.chess;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.pflb.chess.Piece.B_ROOK;
+import static ru.pflb.chess.Square.A2;
+import static ru.pflb.chess.Square.A8;
 
 /**
  * @author <a href="mailto:8445322@gmail.com">Ivan Bonkin</a>.
@@ -18,6 +23,15 @@ public class SearchTest {
         int movesNb = Search.perft(board, 2);
 
         assertThat(movesNb).isEqualTo(65);
+    }
+
+    @Test
+    public void searchTest() {
+        Board board = new Board("r2k4/8/8/8/8/8/R7/K7 w - -");
+
+        List<Move> moves = Search.bestMoves(board);
+
+        assertThat(moves.get(0)).isEqualTo(new Move(A2, A8, B_ROOK));
     }
 
 }
